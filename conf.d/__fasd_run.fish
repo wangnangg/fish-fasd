e4cf792c54ad230ad5d06251eb5637ce5a303b1d
Nan Wang <wangnangg@gmail.com>
2020-04-24 16:50:34 -0700

    fix file recording

--- conf.d/__fasd_run.fish
+++ conf.d/__fasd_run.fish
@@ -1,4 +1,7 @@
-  function __fasd_run -e fish_preexec -d "fasd takes record of the directories changed into"
-    command fasd --proc (command fasd --sanitize "$argv") > "/dev/null" 2>&1 &
+function __fasd_run -e fish_preexec
+  if type -q disown
+    command fasd --proc (command fasd --sanitize "$argv" | tr -s ' ' \n) > "/dev/null" 2>&1 &; disown
+  else
+    command fasd --proc (command fasd --sanitize "$argv" | tr -s ' ' \n) > "/dev/null" 2>&1 &
   end
-  
+end
